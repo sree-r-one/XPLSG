75e3b3273074afeb4470c5af1598acd39bb61e66
sree <sreeraj_ec@hotmail.com>
2025-02-03 18:54:39 +0800

    feat(ui): Homepage Updated with responsiveness

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -21,7 +21,7 @@ const Home: React.FC = () => {
       {/* Hero content */}
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div className="text-center">
-          <h1 className="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+          <h1 className="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl sm:font-extrabold">
             Explore Singapore with Custom Itineraries
           </h1>
           <p className="mt-8 text-lg font-medium text-gray-500 sm:text-xl">
