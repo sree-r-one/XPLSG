2e11c9fc38a87743109100a3c757f63bd3deab77
sree <sreeraj_ec@hotmail.com>
2025-02-03 23:34:33 +0800

    feat(ui): Itinerary Builder Updated with description

--- frontend/src/pages/ItineraryBuilder.tsx
+++ frontend/src/pages/ItineraryBuilder.tsx
@@ -8,7 +8,17 @@ import {
 const ItineraryBuilder = () => {
   return (
     <div>
-      ItineraryBuilder
+      <div className="mx-auto max-w-7xl items-center justify-center">
+        <h1 className="m-5 flex items-center justify-center py-4 text-8xl">
+          Itinerary Builder
+        </h1>
+        <p className="m-5 flex content-center items-center justify-center py-4 text-center text-2xl text-gray-500">
+          Plan your perfect trip effortlessly by customizing every detail of
+          your adventure. Whether you’re traveling for relaxation, adventure, or
+          discovery, this tool helps you tailor a personalized itinerary to
+          match your interests and budget.
+        </p>
+      </div>
       <TravelerSelector />
       <TravelDates />
       <Budget />
