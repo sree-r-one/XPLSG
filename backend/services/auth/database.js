ff56ac18704075038cd295f6f957ff0c24fab4a5
sree <sreeraj_ec@hotmail.com>
2025-02-28 13:47:52 +0800

    feat(mongodb): Starting MOngoDB implementation

---
+++ backend/services/auth/database.js
@@ -0,0 +1,19 @@
+require("dotenv").config({ path: "../../.env" });
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("MongoDB Connection Success");
+  } catch (error) {
+    console.log("MongoDB Connection Failed", error);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
